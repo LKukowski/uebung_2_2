17ca2149233fcfa7d0dc9416c7de48865312417f
LKukowski <lukas.kukowski@nordakademie.de>
2015-10-20 10:42:41 +0200

    Übung 1. BinomischeFormel umgesetzt

---
+++ src/BinomischeFormel.java
@@ -0,0 +1,15 @@
+
+public class BinomischeFormel {
+
+	public static void main(String[] args) {
+	
+		double a = 8;
+		double b = 3;
+		
+	double loesung = (a*a) + (2*a*b) + (b*b);
+	
+	System.out.println("a²+b²= " + loesung);
+
+	}
+
+}
